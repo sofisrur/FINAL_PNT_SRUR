bbd61be9e590069b29edb3ff948bcad3fad19a3d
sofisrur <sofisrur1@gmail.com>
2022-07-12 20:26:28 -0300

    finalpnt2

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container-fluid mt-3">
+    <div class="jumbotron">
+      <hr>
+      <Texto/>
+    </div>
+   </div>
+</template>
+
+<script>
+  import Texto from './components/Texto.vue';
+
+export default {
+
+  name: 'App',
+  components: {
+    Texto
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Texto.vue
@@ -0,0 +1,82 @@
+<template>
+
+  <section class="src-components-texto">
+
+    Color: <input type="text" v-model="color">
+    Fondo: <input type="text" v-model="fondo">
+    Size: <input type="text" v-model="size">
+    <hr>
+    Texto: <input type="text" :style="{color: color, background:fondo, fontSize: size}" v-model="ingreso">
+    <p>{{contar}}</p>  
+
+
+
+  <table>
+    <tr>
+            <th>Numero de Pregunta</th>
+            <th>Respuesta</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>B</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>C</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>B</td>
+          </tr>
+           <tr>
+            <td>4</td>
+            <td>D</td>
+          </tr>
+           <tr>
+            <td>5</td>
+            <td>B, D</td>
+          </tr>
+        </table>
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-components-texto',
+    props: [],
+    mounted () {
+
+    },
+    
+    
+    data () {
+      return {
+        ingreso : " ",
+        color : " ",
+        fondo:" ",
+        size: " "
+      }
+    },
+    methods: {
+       noEsp: function(value){
+            return !value.includes(" ")
+        }
+    
+    },
+    computed: {
+       contar:function(){
+        var resultado = this.ingreso.split(" ")
+        return "Cantidad de palabras: " + resultado.length
+      },
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
